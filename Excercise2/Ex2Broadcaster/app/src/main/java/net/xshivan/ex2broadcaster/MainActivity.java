47ac467bc30798ad45543cb0344b314920d449f0
unknown <michal.cywinski@M-CYWINSKI-10.mddp.pl>
2016-11-15 21:55:48 +0100

    Implementacja logiki aplikacji Broadcastera oraz pusta aplikacja Receivera.

---
+++ Excercise2/Ex2Broadcaster/app/src/main/java/net/xshivan/ex2broadcaster/MainActivity.java
@@ -0,0 +1,22 @@
+package net.xshivan.ex2broadcaster;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void handleBtnSendClick(View view) {
+        String message = ((EditText)findViewById(R.id.txtBoxMessage)).getText().toString();
+        Intent intent = new Intent(message);
+        sendBroadcast(intent, "net.xshivan.permissions.NOTIFY_BROADCAST_SENT");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
