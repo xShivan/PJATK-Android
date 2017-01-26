2b58e05936c01f0eaf0b13f189db275aefbdc2a1
Michał Cywiński <mcywinski@hotmail.com>
2017-01-26 22:29:56 +0100

    Odtwarzanie dźwięki w widżecie.

--- Excercise5/app/src/main/java/net/xshivan/excercise5/MyAppWidget.java
+++ Excercise5/app/src/main/java/net/xshivan/excercise5/MyAppWidget.java
@@ -5,32 +5,54 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class MyAppWidget extends AppWidgetProvider {
+
+    private final String INTENT_SOUND = "net.xshivan.excercise5.INTENT_SOUND";
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,int[] appWidgetIds) {
 
         String aaa = "";
         String bbb = "";
 
-        for(int i=0; i<appWidgetIds.length; i++){
+        for (int i = 0; i < appWidgetIds.length; i++){
             int currentWidgetId = appWidgetIds[i];
-            String url = "http://www.tutorialspoint.com";
+            String url = "http://www.michal-cywinski.pl";
 
             Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setData(Uri.parse(url));
 
             PendingIntent pending = PendingIntent.getActivity(context, 0,intent, 0);
-            RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.activity_main);
+            RemoteViews views = new RemoteViews(context.getPackageName(),R.layout.my_app_widget);
 
-            views.setOnClickPendingIntent(R.id.button, pending);
+            views.setOnClickPendingIntent(R.id.btnWeb, pending);
+            views.setOnClickPendingIntent(R.id.btnSound, getPendingSelfIntent(context, INTENT_SOUND));
+
             appWidgetManager.updateAppWidget(currentWidgetId,views);
             Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (INTENT_SOUND.equals(intent.getAction())) {
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.sound);
+            mp.start();
+        }
+    }
+
+    protected PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
 
